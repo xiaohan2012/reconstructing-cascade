799cc25f6620c9aff269192d731784cd42db9553
Han Xiao <han.xiao@aalto.fi>
2017-10-05 21:51:35 +0000

    result_dir fixed

--- scripts/gen_real_cascade_experiment_cmds.sh
+++ scripts/gen_real_cascade_experiment_cmds.sh
@@ -2,10 +2,10 @@
 
 methods=(closure tbfs no-order)
 qs=(0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1)
-k=10
+k=8
 
 for q in $qs; do
     for method in $methods; do
-	print "python real_cascade_experiment.py  -m ${method}  -q ${q} -k ${k} -o outputs/real_cascade_experiment/${method}/${q}/"
+	print "python real_cascade_experiment.py  -m ${method}  -q ${q} -k ${k} -o outputs/real_cascade_experiment"
     done
 done
